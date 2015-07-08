52c746c72d8aee087f9e549cc15b45426046aa09
Manu <emmanuel.champetier@gmail.com>
2015-07-08 11:28:04 +0200

    Test3

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -8,8 +8,9 @@ namespace TestGit
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //Test
         }
     }
 }
